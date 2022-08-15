e4b837eb5e35833ec72a0c2c38767cd9081bf0e5
Pabloc-43 <pablonicolascrodriguez@gmail.com>
2022-08-14 22:26:18 -0300

    agregando HeroSection beta

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useRef, useEffect, useState } from 'react';
 import './App.css';
 import './components/css/style.css'
 import Header from './components/Header';
+import HeroSection from './components/HeroSection';
 
 function App() {
   
   return (
     <>
-    <Header ></Header>
+    <Header />
+    <HeroSection />
     <div   className='mt-5 d-block' style={{'height': '10000px', 'paddingTop':'100px'}}>
       <h1 className='mt-5'>Contenido</h1>
     </div>
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+        <section id="Inicio" className="container container-xs my-5 py-5" >
+            <div className="row espacioR">
+                <div className="col-xs-12 col-lg-5 d-flex flex-column justify-content-end align-items-start mb-5 ps-0">
+                    <div className="d-flex flex-column ps-2 mobil">
+                        <h1 className="">Hola soy Pablo</h1>
+                        <h3>Estudiante de programacion</h3>
+                        <div id="google_translate_element" className="google"></div>
+                    </div>
+                    <div className="mt-3 botonesJ">
+                        <button className="btn"><a href="#Contacto" className="btn btn-primary">Contactame</a></button>
+                        <button className="btn"><a href="cv.html" className="btn btn-primary" target="_blank">Ver CV</a></button>
+                    </div>
+                </div>
+                <div className="col col-lg-7 col-xs-12">
+                    <img src="./images/TICDay-10.jpg" alt="Foto perfil" className="imagen" />
+                    <img src="./images/Pres.png" alt="Foto perfil" className="imagen" />
+                </div>
+            </div>
+        </section>
+  )
+}
