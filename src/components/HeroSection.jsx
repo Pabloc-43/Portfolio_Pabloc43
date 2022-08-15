d7bba700b8e3aff578ad47ec10ab555ed8259787
Pabloc-43 <pablonicolascrodriguez@gmail.com>
2022-08-14 23:13:11 -0300

    Hero casi responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
         <section id="Inicio" className="container container-xs my-5 py-5" >
             <div className="row espacioR">
-                <div className="col-xs-12 col-lg-5 d-flex flex-column justify-content-end align-items-start mb-5 ps-0">
+                <div className="order-1 col-12 col col-md-5 col-lg-5 d-flex flex-column justify-content-end align-items-start mb-5 ps-0">
                     <div className="d-flex flex-column ps-2 mobil">
                         <h1 className="">Hola soy Pablo</h1>
                         <h3>Estudiante de programacion</h3>
@@ -15,9 +15,8 @@ export default function HeroSection() {
                         <button className="btn"><a href="cv.html" className="btn btn-primary" target="_blank">Ver CV</a></button>
                     </div>
                 </div>
-                <div className="col col-lg-7 col-xs-12">
+                <div className="order-md-1 col-md-7 col-lg-7 col-12">
                     <img src="./images/TICDay-10.jpg" alt="Foto perfil" className="imagen" />
-                    <img src="./images/Pres.png" alt="Foto perfil" className="imagen" />
                 </div>
             </div>
         </section>
