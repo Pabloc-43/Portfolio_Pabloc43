da7708c15b5a43c72ae00cd854a058f1d8fc5922
Pabloc-43 <pablonicolascrodriguez@gmail.com>
2022-08-25 16:55:45 -0300

    Logos conocimientos

--- src/components/data/conocimientos.jsx
+++ src/components/data/conocimientos.jsx
@@ -1,51 +1,52 @@
 const conocimientos = [
     {
         id: 1,
-        title: "Billie Eilish",
+        title: "HTML 5",
         descripcion: "la falopa hace mal che!",
         imagen: "images/html-5.png",
         claseCSS: 'col col-lg-3 col-xs-12 col-md-6 habil'
     },
+
     {
         id: 2,
-        title: "Billie Eilish",
-        descripcion: "la falopa hace mal che!",
-        imagen: "images/piton.png",
-        claseCSS: 'col col-lg-3 col-xs-12 col-md-6 habil'
-    },
-    {
-        id: 3,
-        title: "Billie Eilish",
+        title: "CSS 3",
         descripcion: "la falopa hace mal che!",
         imagen: "images/css-3.png",
         claseCSS: 'col col-lg-3 col-xs-12 col-md-6 habil'
     },
     {
-        id: 4,
-        title: "Billie Eilish",
+        id: 3,
+        title: "React",
         descripcion: "la falopa hace mal che!",
-        imagen: "images/figma.png",
+        imagen: "images/react.png",
+        claseCSS: 'col col-lg-3 col-xs-12 col-md-6 habil'
+    },
+    {
+        id: 4,
+        title: "JS",
+        descripcion: "la falopa hace mal che!",
+        imagen: "images/js.png",
         claseCSS: 'col col-lg-3 col-xs-12 col-md-6 habil'
     },
     {
         id: 5,
-        title: "Billie Eilish",
+        title: "Python 3",
         descripcion: "la falopa hace mal che!",
-        imagen: "images/js.png",
+        imagen: "images/piton.png",
         claseCSS: 'col col-lg-4 col-xs-12 habil col-md-6'
     },
     {
         id: 6,
-        title: "Billie Eilish",
+        title: "SQL",
         descripcion: "la falopa hace mal che!",
-        imagen: "images/cad-file.png",
+        imagen: "images/sql.png",
         claseCSS: 'col col-lg-4 col-xs-12 habil col-md-6'
     },
     {
         id: 7,
-        title: "Billie Eilish",
+        title: "Flask",
         descripcion: "la falopa hace mal che!",
-        imagen: "images/html-5.png",
+        imagen: "images/flask.png",
         claseCSS: 'col col-lg-4 col-xs-12 habil col-md-6'
     }
 ]
